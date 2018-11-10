1f771f069d81b736635aafdead1e7675760bacd6
Zenno Bruinsma <zenno.bruinsma@bloomon.nl>
2018-11-10 21:19:42 +0100

    implemented timer

--- app/components/Popup/index.js
+++ app/components/Popup/index.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Line } from 'rc-progress';
+
 import './styles.scss';
 
 class Popup extends Component {
@@ -6,20 +8,65 @@ class Popup extends Component {
     super(props);
     this.state = {
       visible: true,
+      seconds: 0,
+      maxSeconds: 7,
     };
+  }
+
+  // componentDidMount() {
+  //   const { seconds } = this.state;
+  //   if (seconds >= 7) {
+  //     this.interval = setInterval(() => this.setState({ seconds: Date.now() }), 1000);
+  //   }
+  // }
+
+  // componentWillUnmount() {
+  //   clearInterval(this.interval);
+  // }
+
+  componentDidMount() {
+    this.interval = setInterval(() => this.tick(), 10);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
   }
 
   open = () => this.setState({ visible: true });
 
+  tick() {
+    this.setState(prevState => ({
+      seconds: prevState.seconds + 0.01,
+    }));
+  }
+
   /* eslint-disable */
   render() {
-    const { visible } = this.state;
+    const { visible, seconds, maxSeconds } = this.state;
     const { content } = this.props;
+
+    // const Timer = props => {
+    //   const { seconds, maxSeconds } = this.props;
+    //   const styles = {
+    //     width: (seconds / maxSeconds) * 100,
+    //   };
+    //   return <div className="timer" style={styles} />;
+    // };
     if (visible) {
       return (
         <div className="popup">
           <div className="popup_inner">
-            <h4>Nike is an American corporation specializing in the sale of sports equipment. In which state would you find their headquarters?</h4>
+            <Line
+              percent={(seconds / maxSeconds) * 100}
+              strokeWidth="4"
+              strokeColor="#000"
+              strokeLinecap="square"
+            />
+
+            <h4>
+              Nike is an American corporation specializing in the sale of sports equipment. In which
+              state would you find their headquarters?
+            </h4>
             {content}
             <button
               type="button"
